1b4f2758748eb0a11c2d1f1269c218adee4dd45c
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-02-16 00:46:25 +0000

    style refactoring

--- management/commands/glue.py
+++ management/commands/glue.py
@@ -1,6 +1,4 @@
 # coding: UTF-8
-
-
 import os
 import shutil
 import subprocess
@@ -12,49 +10,35 @@ from django.utils.translation import ugettext as _
 
 
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
         conf = settings.GLUE_CONFIG
         command = ['glue', conf['source'], conf['output'], '--recursive', '--project', ]
         ext = '.css'
-
-
         if conf.get('less', False):
             command += ['--less']
             ext = '.less'
-
         if conf.get('scss', False):
             command += ['--scss']
             ext = '.scss'
-
         if conf.get('css_url', False):
             command += ['--url=%s' % conf['css_url']]
-
         if conf.get('crop', False):
             command += ['--crop']
-
         if conf.get('margin', False):
             command += ['--margin=%d' % conf['margin']]
-
-
         try:
             subprocess.call(command)
         except OSError:
             raise Exception(_('glue does not exists on your os'))
         else:
-
             if not os.path.exists(conf['move_styles_to']):
                 os.makedirs(conf['move_styles_to'])
-
             for basename in os.listdir(conf['output']):
-
                 if basename.endswith(ext):
                     pathname = os.path.join(conf['output'], basename)
                     dstdir = os.path.join(conf['move_styles_to'], basename)
-
                     if os.path.isfile(pathname):
                         shutil.move(pathname, dstdir)
-
                         if conf.get('csscomb', False):
                             try:
                                 if conf.get('csscomb_bin', False):
